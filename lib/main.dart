7af119a71303342d51901fa701042cb5b7e0da38
Geoffrey Kim <76798197+mochafreddo@users.noreply.github.com>
2023-09-17 19:54:11 +0900

    refactor: remove Google fonts and prepare for future design updates
    Removed the usage of Google fonts from the application. This change is in preparation for upcoming design modifications. Further design adjustments will be made as needed to ensure a consistent and visually appealing user experience.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_onboarding_flow/initial_screen.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 void main() {
-  // Ensure that widget binding is initialized
   WidgetsFlutterBinding.ensureInitialized();
 
   runApp(const OnboardingFlowPartOne());
@@ -12,7 +10,6 @@ void main() {
 class OnboardingFlowPartOne extends StatelessWidget {
   const OnboardingFlowPartOne({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -20,12 +17,12 @@ class OnboardingFlowPartOne extends StatelessWidget {
       home: const InitialScreen(),
       theme: ThemeData(
         useMaterial3: true,
-        fontFamily: GoogleFonts.roboto().fontFamily,
         scaffoldBackgroundColor: Colors.white,
         appBarTheme: const AppBarTheme(
           backgroundColor: Colors.white,
           elevation: 0.0,
           centerTitle: true,
+          scrolledUnderElevation: 0.0,
         ),
       ),
     );
